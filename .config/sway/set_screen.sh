b9c88e262e187b46e9a5ee7a85522f58ebb9984b
Alexander <terminalnode@protonmail.com>
2019-05-22 09:53:41 +0200

    sway/set_screen.sh: shellchecked.

--- .config/sway/set_screen.sh
+++ .config/sway/set_screen.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 # Stop script if no input.
-if [ -z $1 ]; then exit 33; fi
+if [ -z "$1" ]; then exit 33; fi
 
 # Check maximum and current values.
 LIGHT="/sys/class/backlight/intel_backlight"
@@ -10,11 +10,11 @@ NOW=$( cat $LIGHT/brightness )
 # Calculate desired brightness as percentage of MAX.
 # Then remove trailing decimals.
 DESIRED=$(echo "scale=2; $MAX / 100 * $1" | bc | sed "s/\..*//")
-DESIRED=$(($NOW + $DESIRED))
+DESIRED=$((NOW + DESIRED))
 
 # If DESIRED would go above max or below 0, set to max or 0.
 [ "$DESIRED" -gt "$MAX" ] && DESIRED=$MAX
 [ "$DESIRED" -lt 0 ] && DESIRED=0
 
 # Send the result to brightness.
-echo $DESIRED >> $LIGHT/brightness
+echo "$DESIRED" >> "$LIGHT"/brightness
