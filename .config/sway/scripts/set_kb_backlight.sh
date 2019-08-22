dc31fa5d9761e2ce497881604a0cb3756ff03804
Alexander <terminalnode@protonmail.com>
2019-08-22 18:37:17 +0200

    newsboat: lots of changes to keybindings

---
+++ .config/sway/scripts/set_kb_backlight.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+# This is basically the screen brightness script with different inputs and outputs
+
+# Stop script if no input
+if [ -z "$1" ]; then exit 33; fi
+
+# Check maximum and current values.
+LIGHT="/sys/class/leds/smc::kbd_backlight"
+MAX=$( cat $LIGHT/max_brightness )
+NOW=$( cat $LIGHT/brightness )
+
+# Calculate desired brightness as percentage of MAX.
+# Then remove trailing decimals.
+DESIRED=$(echo "scale=2; $MAX / 100 * $1" | bc | sed "s/\..*//")
+DESIRED=$((NOW + DESIRED))
+
+# If DESIRED would go above max or below 0, set to max or 0.
+[ "$DESIRED" -gt "$MAX" ] && DESIRED=$MAX
+[ "$DESIRED" -lt 0 ] && DESIRED=0
+
+# Send the result to brightness.
+echo "$DESIRED" >> "$LIGHT"/brightness
