ddc32e497a2f4ae41416f7f52d3f79988f08ba49
Alexander <2683344+terminalnode@users.noreply.github.com>
2020-06-08 20:34:34 +0200

    make dwm keybinds more similar to my sway conf

--- suckless/dwm/config.h
+++ suckless/dwm/config.h
@@ -38,10 +38,10 @@ static const int nmaster     = 1;    /* number of clients in master area */
 static const int resizehints = 1;    /* 1 means respect size hints in tiled resizals */
 
 static const Layout layouts[] = {
-  /* symbol     arrange function */
-  { "[]=",      tile },    /* first entry is default */
-  { "><>",      NULL },    /* no layout function means floating behavior */
-  { "[M]",      monocle },
+  /* symbol       arrange function */
+  { "[tiling]",   tile },    /* first entry is default */
+  { "[floating]", NULL },    /* no layout function means floating behavior */
+  { "[monocle]",  monocle }, /* show only one window */
 };
 
 /* key definitions */
@@ -61,40 +61,50 @@ static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont,
 static const char *termcmd[]  = { "st", NULL };
 
 static Key keys[] = {
-  /* modifier                     key        function        argument */
-  { MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
-  { MODKEY|ShiftMask,             XK_Return, spawn,          {.v = termcmd } },
-  { MODKEY,                       XK_b,      togglebar,      {0} },
-  { MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
-  { MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
-  { MODKEY,                       XK_i,      incnmaster,     {.i = +1 } },
-  { MODKEY,                       XK_d,      incnmaster,     {.i = -1 } },
-  { MODKEY,                       XK_h,      setmfact,       {.f = -0.05} },
-  { MODKEY,                       XK_l,      setmfact,       {.f = +0.05} },
-  { MODKEY,                       XK_Return, zoom,           {0} },
-  { MODKEY,                       XK_Tab,    view,           {0} },
-  { MODKEY|ShiftMask,             XK_c,      killclient,     {0} },
-  { MODKEY,                       XK_t,      setlayout,      {.v = &layouts[0]} },
-  { MODKEY,                       XK_f,      setlayout,      {.v = &layouts[1]} },
-  { MODKEY,                       XK_m,      setlayout,      {.v = &layouts[2]} },
-  { MODKEY,                       XK_space,  setlayout,      {0} },
-  { MODKEY|ShiftMask,             XK_space,  togglefloating, {0} },
-  { MODKEY,                       XK_0,      view,           {.ui = ~0 } },
-  { MODKEY|ShiftMask,             XK_0,      tag,            {.ui = ~0 } },
-  { MODKEY,                       XK_comma,  focusmon,       {.i = -1 } },
-  { MODKEY,                       XK_period, focusmon,       {.i = +1 } },
-  { MODKEY|ShiftMask,             XK_comma,  tagmon,         {.i = -1 } },
-  { MODKEY|ShiftMask,             XK_period, tagmon,         {.i = +1 } },
-  TAGKEYS(                        XK_1,                      0)
-  TAGKEYS(                        XK_2,                      1)
-  TAGKEYS(                        XK_3,                      2)
-  TAGKEYS(                        XK_4,                      3)
-  TAGKEYS(                        XK_5,                      4)
-  TAGKEYS(                        XK_6,                      5)
-  TAGKEYS(                        XK_7,                      6)
-  TAGKEYS(                        XK_8,                      7)
-  TAGKEYS(                        XK_9,                      8)
-  { MODKEY|ShiftMask,             XK_q,      quit,           {0} },
+  /* Basic navigation */
+  /* See TAGKEYS definition above */
+  TAGKEYS(XK_1, 0)
+  TAGKEYS(XK_2, 1)
+  TAGKEYS(XK_3, 2)
+  TAGKEYS(XK_4, 3)
+  TAGKEYS(XK_5, 4)
+  TAGKEYS(XK_6, 5)
+  TAGKEYS(XK_7, 6)
+  TAGKEYS(XK_8, 7)
+  TAGKEYS(XK_9, 8)
+
+  /* Important options */
+  /* modifier                     key         function        argument */
+  { MODKEY,                       XK_space,   spawn,          {.v = dmenucmd } },
+  { MODKEY,                       XK_Return,  spawn,          {.v = termcmd } },
+  { MODKEY|ShiftMask,             XK_Return,  zoom,           {0} },
+  { MODKEY,                       XK_Tab,     view,           {0} },
+  { MODKEY|ShiftMask,             XK_q,       quit,           {0} },
+  { MODKEY,                       XK_p,       setlayout,      {0} },
+  { MODKEY|ShiftMask,             XK_p,       togglefloating, {0} },
+  /* Change which item in the stack to focus */
+  { MODKEY,                       XK_j,       focusstack,     {.i = +1 } },
+  { MODKEY,                       XK_k,       focusstack,     {.i = -1 } },
+  /* Change monitor focus */
+  { MODKEY,                       XK_comma,   focusmon,       {.i = -1 } },
+  { MODKEY,                       XK_period,  focusmon,       {.i = +1 } },
+  /* Move focused window to other monitor */
+  { MODKEY|ShiftMask,             XK_comma,   tagmon,         {.i = -1 } },
+  { MODKEY|ShiftMask,             XK_period,  tagmon,         {.i = +1 } },
+  /* Promote and depromote windows to master */
+  { MODKEY,                       XK_i,       incnmaster,     {.i = +1 } },
+  { MODKEY,                       XK_d,       incnmaster,     {.i = -1 } },
+
+  /* Unsorted options stuff */
+  { MODKEY,                       XK_b,       togglebar,      {0} },
+  { MODKEY,                       XK_h,       setmfact,       {.f = -0.05} },
+  { MODKEY,                       XK_l,       setmfact,       {.f = +0.05} },
+  { MODKEY|ShiftMask,             XK_c,       killclient,     {0} },
+  { MODKEY,                       XK_t,       setlayout,      {.v = &layouts[0]} },
+  { MODKEY,                       XK_f,       setlayout,      {.v = &layouts[1]} },
+  { MODKEY,                       XK_m,       setlayout,      {.v = &layouts[2]} },
+  { MODKEY,                       XK_0,       view,           {.ui = ~0 } },
+  { MODKEY|ShiftMask,             XK_0,       tag,            {.ui = ~0 } },
 };
 
 /* button definitions */
