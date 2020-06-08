5fd4157f72e6fe13f0f8ed5d72387a78433b30e0
Alexander <2683344+terminalnode@users.noreply.github.com>
2020-06-08 21:40:44 +0200

    dwm: apply nord-esque theme

--- suckless/dwm/config.h
+++ suckless/dwm/config.h
@@ -1,20 +1,37 @@
 /* See LICENSE file for copyright and license details. */
 /* appearance */
-static const unsigned int borderpx  = 1;        /* border pixel of windows */
+static const unsigned int borderpx  = 2;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const char *fonts[]          = { "monospace:size=10" };
 static const char dmenufont[]       = "monospace:size=10";
-static const char col_gray1[]       = "#222222";
-static const char col_gray2[]       = "#444444";
-static const char col_gray3[]       = "#bbbbbb";
-static const char col_gray4[]       = "#eeeeee";
-static const char col_cyan[]        = "#005577";
+
+/*
+ * Nord color definitions
+ * Probably not gonna use all of them, but they're
+ * good to have on hand for testing and such.
+ */
+static const char polar1[]          = "#2e3440";
+static const char polar2[]          = "#3b4252";
+static const char polar3[]          = "#434c5e";
+static const char polar4[]          = "#3c566a";
+static const char snow1[]           = "#d8dee9";
+static const char snow2[]           = "#e5e9f0";
+static const char snow3[]           = "#eceff4";
+static const char frost1[]          = "#8fbcbb";
+static const char frost2[]          = "#88c0d0";
+static const char frost3[]          = "#81a1c1";
+static const char frost4[]          = "#5e81ac";
+static const char red[]             = "#bf616a";
+static const char orange[]          = "#d08770";
+static const char yellow[]          = "#ebcb8b";
+static const char green[]           = "#a3be8c";
+static const char magenta[]         = "#b48ead";
 static const char *colors[][3]      = {
-  /*               fg           bg          border   */
-  [SchemeNorm] = { col_gray3,   col_gray1,  col_gray2 },
-  [SchemeSel]  = { col_gray4,   col_cyan,   col_cyan  },
+  /*               fg       bg        border   */
+  [SchemeNorm] = { snow1,   polar1,   polar2 },
+  [SchemeSel]  = { snow3,   polar4,   frost4 },
 };
 
 /* tagging */
@@ -58,7 +75,7 @@ static const Layout layouts[] = {
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
+static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", polar1, "-nf", snow1, "-sb", frost4, "-sf", snow3, NULL };
 static const char *termcmd[]  = { "st", NULL };
 
 static Key keys[] = {
