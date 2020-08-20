b92473e97f82e9532db24cc74bb61382433309f8
Alexander <2683344+terminalnode@users.noreply.github.com>
2020-08-20 21:07:56 +0200

    qb: finalize reddit-remove-promoted.js
    There may still be improvements, but right now this script seems to be
    doing exactly what I want it to.

--- .local/share/qutebrowser/greasemonkey/reddit-remove-promoted.js
+++ .local/share/qutebrowser/greasemonkey/reddit-remove-promoted.js
@@ -1,14 +1,39 @@
 // ==UserScript==
 // @description Remove promoted links and ads on reddit.
-// @include     https://www.reddit.com/
-// @version     0.1
+// @include     https://www.reddit.com/*
+// @version     1.0
 // ==/UserScript==
 
-console.log("UserScript Reddit Remove Promoted");
+console.log("UserScript Reddit Remove Promoted: Started");
 
-// Delete the reddit premium element
-const premiumBanner = document.getElementsByClassName("premium-banner-outer");
-if (premiumBanner.length > 0) {
-  console.log("Removing premium ad")
-  premiumBanner[0].parentNode.removeChild(premiumBanner[0]);
+// Find all elements with a matching class and delete them
+function removeElementsByClassName(className) {
+  let collection = document.getElementsByClassName(className);
+  let removed = 0;
+
+  while (collection.length > 0) {
+    console.log(`Removing element from ${className}, number ${++removed}`)
+    let element = collection.item(0);
+    element.parentNode.removeChild(element)
+  }
 }
+
+// Find all elements with a matching class,
+// find their parents, remove their parents.
+// The child can't live without it's parent.
+function removeElementsParentsByClassName(className) {
+  let collection = document.getElementsByClassName(className);
+  let removed = 0;
+
+  while (collection.length > 0) {
+    console.log(`Removing element from ${className}, number ${++removed}`)
+    let element = collection.item(0);
+    element.parentNode.parentNode.removeChild(element.parentNode)
+  }
+}
+
+removeElementsByClassName("promotedlink")
+removeElementsParentsByClassName("premium-banner-outer")
+removeElementsParentsByClassName("ad-container")
+
+console.log("UserScript Reddit Remove Promoted: Finished")
