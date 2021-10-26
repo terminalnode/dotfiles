8cf5f80301554e7f04defc95bd585f5cfe762fe1
Alexander Rundberg <alexanderrundberg@fastmail.jp>
2021-10-26 21:43:23 +0200

    decimal_time.cr: add formatting options

--- src/decimal_time.cr
+++ src/decimal_time.cr
@@ -6,9 +6,37 @@
 # Seconds in a decimal hour:    8640
 # Seconds in a decimal minute:  86.4
 # The decimals of the decimal minute will be used as decimal seconds.
+mode = ARGV[0]? || "long"
+
 now = Time.local
 secondsPassed = (now.to_unix + now.offset) % 86400
 decimalHour, secondsPassed = secondsPassed.divmod(8640)
 decimalMinute = secondsPassed / 86.4
 
-puts "#{decimalHour}:#{"%05.2f" % decimalMinute}"
+if mode == "long"
+  puts "#{decimalHour}.#{"%05.2f" % decimalMinute}"
+elsif mode == "short"
+  puts "#{decimalHour}.#{"%02i" % decimalMinute}"
+elsif mode == "hour"
+  puts "%02i" % decimalHour
+elsif mode == "minute"
+  puts "%02i" % decimalMinute.to_i
+elsif mode == "second"
+  puts "%02i" % (decimalMinute / 100).to_s[4, 2]
+elsif mode == "help"
+  puts <<-HELPTEXT
+  Usage: decimal_time [OPTION]
+  Show the current time in decimal time, where we have 10 hours per day and
+  every hour consists of 100 minutes (which can be further divided into 100
+  seconds).
+
+  If no format is specified the default is long.
+
+  OPTION    EXAMPLE     Padding
+  long      1.23.45     Hour is not zero-padded
+  short     1.23        Hour is not zero-padded
+  hour      01          Zero-padded
+  minute    23          Zero-padded
+  second    45          Zero-padded
+  HELPTEXT
+end
