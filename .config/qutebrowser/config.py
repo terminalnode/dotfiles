9d3575905c6c945c6b91d838edcb94281eec990a
Alexander Rundberg <alexanderrundberg@fastmail.jp>
2022-01-12 14:29:19 +0100

    prefer dark mode in qutebrowser

--- .config/qutebrowser/config.py
+++ .config/qutebrowser/config.py
@@ -70,12 +70,13 @@ c.zoom.levels = [
 c.content.cookies.accept = "all"
 
 # Dark mode rocks
+c.colors.webpage.preferred_color_scheme = "dark"
 c.colors.webpage.darkmode.enabled = True
-c.colors.webpage.darkmode.algorithm = 'lightness-hsl'
+c.colors.webpage.darkmode.algorithm = "lightness-hsl"
 c.colors.webpage.darkmode.contrast = -.022
 c.colors.webpage.darkmode.threshold.text = 150
 c.colors.webpage.darkmode.threshold.background = 100
-c.colors.webpage.darkmode.policy.images = 'always'
+c.colors.webpage.darkmode.policy.images = "always"
 c.colors.webpage.darkmode.grayscale.images = 0.35
 
 # Previously enabled options that are disabled now.
