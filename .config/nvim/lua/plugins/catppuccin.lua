14668f5fc25eb5d406686fc78ef2019bf88dfa7a
Alexander Rundberg <alex@gwai.se>
2024-12-28 22:27:05 +0100

    nvim: Add blink.cmp config

--- .config/nvim/lua/plugins/catppuccin.lua
+++ .config/nvim/lua/plugins/catppuccin.lua
@@ -3,6 +3,19 @@ return {
     "catppuccin/nvim",
     priority = 1000,
     name = "catppuccin",
-    config = function() vim.cmd.colorscheme("catppuccin-latte") end,
+    config = function()
+      require("catppuccin").setup({
+        flavour = "latte", -- latte, frappe, macchiato, mocha
+        dim_inactive = { enabled = true },
+        integrations = {
+          blink_cmp = true,
+          noice = true,
+          nvim_surround = true,
+          snacks = true,
+          telescope = { enabled = true },
+        },
+      })
+      vim.cmd.colorscheme("catppuccin-latte")
+    end,
   },
 }
--- .config/nvim/lua/plugins/lsp-config.lua
+++ .config/nvim/lua/plugins/lsp-config.lua
@@ -1,28 +1,59 @@
 return {
   {
+    "saghen/blink.cmp",
+    dependencies = { "rafamadriz/friendly-snippets" },
+
+    version = "*",
+
+    ---@module "blink.cmp"
+    ---@type blink.cmp.Config
+    opts = {
+      keymap = { preset = "default" },
+
+      appearance = {
+        -- Fallback for when your theme doesn't support blink.cmp
+        use_nvim_cmp_as_default = true,
+        -- Set to 'mono' for 'Nerd Font Mono' or 'normal' for 'Nerd Font'
+        nerd_font_variant = "mono"
+      },
+
+      -- Show signature help when passing parameters to a function
+      -- https://cmp.saghen.dev/configuration/signature.html
+      signature = { enabled = true },
+    },
+    opts_extend = { "sources.default" }
+  },
+  {
     "neovim/nvim-lspconfig",
-    config = function()
-      local c = require("lspconfig")
-      c.lua_ls.setup({
-        -- For Lua it's mostly gonna be neovim files, where it's fine not to have the LSP
-        silent = true,
-        settings = {
-          Lua = {
-            diagnostics = {
-              globals = { "vim" },
+    dependencies = { "blink.cmp" },
+    opts = {
+      servers = {
+        lua_ls = {
+          -- For Lua it's usually gonna be neovim files, where it's fine not to have the LSP
+          silent = true,
+
+          settings = {
+            Lua = {
+              workspace = {
+                library = vim.api.nvim_get_runtime_file("", true),
+              }
             },
-            workspace = {
-              library = vim.api.nvim_get_runtime_file("", true),
-            }
           },
         },
-      })
 
-      -- For these languages I kinda want it to warn if server not installed (silent = false)
-      c.clangd.setup({ silent = false })
-      c.hls.setup({ silent = false, filetypes = { "haskell", "lhaskell", "cabal" } })
-      c.nixd.setup({ silent = false })
-      c.rust_analyzer.setup({ silent = false })
+        clangd = { silent = false },
+        hls = { silent = false, filetypes = { "haskell", "lhaskell", "cabal" } },
+        nixd = { silent = false },
+        rust_analyzer = { silent = false },
+      },
+    },
+    config = function(_, opts)
+      local blink = require("blink.cmp")
+      local c = require("lspconfig")
+      for server, config in pairs(opts.servers) do
+        config.capabilities = blink.get_lsp_capabilities(config.capabilities)
+        c[server].setup(config)
+      end
     end,
   },
 }
